5dd6b4d24c9f09a04628729107d7deaf4a6a4a4f
Taylor Dawson <taylorjdawson@gmail.com>
2018-09-26 10:51:43 -0700

    Removed _redirects, add Home comp, added apiKey/blockchainId to state

--- src/store/state.js
+++ src/store/state.js
@@ -2,7 +2,8 @@ import config from './config.json'
 import abi from '../../build/contracts/Example.json'
 
 const constants = {
-  web3data-api: '9bb279c9b9444bf0a94ead7a3768409a',
+  apiKey: '9bb279c9b9444bf0a94ead7a3768409a',
+  blockchainId: '1b3f7a72b3e99c13', //ethereum-rinkeby
   testingAccount: '0xd797d5784f43de300ec0d6bc34fc86023c5d71ce',
   contractAddress: config.address,
   rpcUrl: `https://rinkeby.infura.io`
